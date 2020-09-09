80a4f7908364e7a6b3e75e610f0e40b67bbb7831
Liam Best <liambbest@googlemail.com>
2020-09-09 13:24:52 +0200

    Removed composeEnhancers

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import thunk from "redux-thunk";
 import cartReducer from "./Reducers/cartReducer";
 
 // variable to connect to redux dev tool chrome extension
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 
 // initiate redux store with cartReducer and thunk middleware
 const store = createStore(cartReducer, applyMiddleware(thunk));
